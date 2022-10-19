4b939ac0808f335687dcb20cb28e3bc91df5a3b8
NicholasGodwin <nicholas@techwriteresearcher.com>
2022-10-18 17:06:01 -0700

    Write a function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,30 @@
 #include "main.h"
 
 /**
- * _isalpha - Shows 1 if the input is a
- * letter Another cases, shows 0
+ * print_sign - Determines if the input number
+ * greater, iqual or less than zero.
  *
- * @c: The character in ASCII code
+ * @n: The input number as an integer.
  *
- * Return: 1 for letters. 0 for the rest.
+ * Return: 1 is greater than zero. 0 is zero.
+ * -1 is less than zero.
  */
-int _isalpha(int c)
+int print_sign(int n)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (n > 0)
 	{
+		_putchar(43);
 		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
+		_putchar(48);
 		return (0);
 	}
 	_putchar('\n');
-}
+}	
