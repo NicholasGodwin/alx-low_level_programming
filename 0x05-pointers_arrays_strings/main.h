0454af125f209640e92037d90965d78c7b361acb
NicholasGodwin <nicholas@techwriteresearcher.com>
2022-10-24 12:47:36 -0700

    from tonia's git

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
-void reset_to_98(int *n);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -8,3 +11,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+
+#endif
